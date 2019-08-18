6d665014753f74bce162560f7cb33d2fa187d5c1
Pysuper <1821764535@qq.com>
2019-08-19 01:27:30 +0800

    settings.py配置信息

--- tencent/tencent/settings.py
+++ tencent/tencent/settings.py
@@ -1,95 +1,76 @@
-# -*- coding: utf-8 -*-
-
-# Scrapy settings for tencent project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://doc.scrapy.org/en/latest/topics/settings.html
-#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-
+# 项目名
 BOT_NAME = 'tencent'
 
+# 爬虫位置
 SPIDER_MODULES = ['tencent.spiders']
 NEWSPIDER_MODULE = 'tencent.spiders'
 
-
+# 日志级别
 LOG_LEVEL = "WARNING"
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'tencent (+http://www.yourdomain.com)'
+# 设置默认的用户身份 ==> 自己配置改成浏览器的
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'
 
-# Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+# 是否遵循robots协议, 不让你怕, 是不是就不爬了 ==> 默认True
+ROBOTSTXT_OBEY = False
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# 最大连接数, 默认16, 并发请求数, 默认注释32
+# CONCURRENT_REQUESTS = 5
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# 下载延迟
+# DOWNLOAD_DELAY = 3
 
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# 对每个域名, 同时并发请求数
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
 
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# 对每个IP, 同时并发请求数
+# CONCURRENT_REQUESTS_PER_IP = 16
 
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
+# 是否关闭cookie, 默认开启==>False
+# COOKIES_ENABLED = False
 
-# Enable or disable spider middlewares
-# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'tencent.middlewares.TencentSpiderMiddleware': 543,
-#}
+# 是否开启远程控制
+# TELNETCONSOLE_ENABLED = False
 
-# Enable or disable downloader middlewares
-# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'tencent.middlewares.TencentDownloaderMiddleware': 543,
-#}
+# 默认请求头
+DEFAULT_REQUEST_HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en',
+}
 
-# Enable or disable extensions
-# See https://doc.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# 设置爬虫中间件
+# SPIDER_MIDDLEWARES = {
+# 'tencent.middlewares.TencentSpiderMiddleware': 543,
+# }
 
-# Configure item pipelines
-# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+# 设置下载中间件
+# DOWNLOADER_MIDDLEWARES = {
+# 'tencent.middlewares.TencentDownloaderMiddleware': 543,
+# }
+
+# 设置扩展
+# EXTENSIONS = {
+# 'scrapy.extensions.telnet.TelnetConsole': None,
+# }
+
+# 设置管道
 ITEM_PIPELINES = {
-   'tencent.pipelines.TencentPipeline': 300,
-   'tencent.pipelines.MongodbListItemPipeline': 301,
-   'tencent.pipelines.MongodbDetailItemPipeline': 302,
+    'tencent.pipelines.TencentPipeline': 300,
+    'tencent.pipelines.MongodbListItemPipeline': 301,
+    'tencent.pipelines.MongodbDetailItemPipeline': 302,
 
 }
 
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
+# 设置用户名和密码
+# AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_DEBUG = False
 
-# Enable and configure HTTP caching (disabled by default)
-# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# 是否缓存请求
+HTTPCACHE_ENABLED = True
+HTTPCACHE_EXPIRATION_SECS = 0
+HTTPCACHE_DIR = 'httpcache' # 本地 ==> 当前文件夹下.scrapy文件夹中的httpcache中, 下次对于相同域名, 先从缓存中提取请求
+HTTPCACHE_IGNORE_HTTP_CODES = [403, 404, 500, ] # 哪些 请求状态 数不需要缓存的
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
